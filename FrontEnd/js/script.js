764a652930fd66c4da77af917d3a1f60089c6f3b
Hatem Abdoulatuf <abdoulatuf.pro@gmail.com>
2022-12-05 19:58:33 +0100

    (add):filtre function

--- FrontEnd/js/script.js
+++ FrontEnd/js/script.js
@@ -2,38 +2,53 @@ const gallery = document.getElementById("gallery");
 const URL = "http://localhost:5678/api/works";
 
 // Récupération des données de l'API
- fetch(URL)
-    .then(function(res){
-        if(res.ok){
-            return res.json();
-        } else {
-            alert('La requete a échoué !');
-        }
-    })
-    .then(function(data){
-        for(let projets of data){
-            gallery.innerHTML += `
-                <figure class="item" data-item="${projets.category.name}">
+fetch(URL)
+  .then(function (res) {
+    if (res.ok) {
+      return res.json();
+    } else {
+      alert("La requete a échoué !");
+    }
+  })
+  .then(function (data) {
+    for (let projets of data) {
+      gallery.innerHTML += `
+                <figure class="item" category="${projets.category.name}">
                     <img src="${projets.imageUrl}" alt="${projets.title}">
                      <figcaption>${projets.title}</figcaption>
-                </figure>`
-        }
-    })
+                </figure>`;
+    }
+  })
+  .then(function(){
+        const liItem = document.querySelectorAll("#theFiltre li");
+      const imgItem = document.querySelectorAll("#gallery figure");
+
+      liItem.forEach((li) => {
+        li.onclick = function () {
+          //Filter
+          const value = li.textContent;
+          imgItem.forEach((figure) => {
+            figure.style.display = "none";
+            if(figure.getAttribute("category") == value ||value == "All Menu"
+            ) {
+              figure.style.display = "block";
+            }
+          });
+        };
+      });
+  })
 
 
-//** */ Le filtrage des projets par catégories$
-let btnFiltre = document.querySelectorAll('.filtre');
-let items = document.querySelectorAll('.item');
+// ********** */
+let btnFiltre = document.querySelectorAll(".filtre");
+let items = document.querySelectorAll(".item");
 
 console.log(items);
-for(let i = 0; i < btnFiltre.length; i++){
-    btnFiltre[i].addEventListener('click', function(){
-        for (let j = 0; j < btnFiltre.length; j++) {
-            btnFiltre[j].classList.remove('active');
-        }
-        this.classList.add('active');
-
-        console.log(items.textContent);
-    })
+for (let i = 0; i < btnFiltre.length; i++) {
+  btnFiltre[i].addEventListener("click", function () {
+    for (let j = 0; j < btnFiltre.length; j++) {
+      btnFiltre[j].classList.remove("active");
+    }
+    this.classList.add("active");
+  });
 }
-
