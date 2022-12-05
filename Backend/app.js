363d65d3585973e733aef101643c23b6cb552ec9
Hatem Abdoulatuf <abdoulatuf.pro@gmail.com>
2022-12-05 19:31:09 +0100

    création de la page de connexion

--- Backend/app.js
+++ Backend/app.js
@@ -10,7 +10,7 @@ const app = express()
 app.use(cors())
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
-app.use(helmet());
+// app.use(helmet());
 app.use('/images', express.static(path.join(__dirname, 'images')))
 
 const db = require("./models");
--- FrontEnd/js/script.js
+++ FrontEnd/js/script.js
@@ -1,6 +1,39 @@
-const gallery = document.getElementById("gallery")
+const gallery = document.getElementById("gallery");
+const URL = "http://localhost:5678/api/works";
+
+// Récupération des données de l'API
+ fetch(URL)
+    .then(function(res){
+        if(res.ok){
+            return res.json();
+        } else {
+            alert('La requete a échoué !');
+        }
+    })
+    .then(function(data){
+        for(let projets of data){
+            gallery.innerHTML += `
+                <figure class="item" data-item="${projets.category.name}">
+                    <img src="${projets.imageUrl}" alt="${projets.title}">
+                     <figcaption>${projets.title}</figcaption>
+                </figure>`
+        }
+    })
 
 
-fetch("http://localhost:5678/api/works")
-    .then(res => res.json())
-    .then(data => console.log(data))
+//** */ Le filtrage des projets par catégories$
+let btnFiltre = document.querySelectorAll('.filtre');
+let items = document.querySelectorAll('.item');
+
+console.log(items);
+for(let i = 0; i < btnFiltre.length; i++){
+    btnFiltre[i].addEventListener('click', function(){
+        for (let j = 0; j < btnFiltre.length; j++) {
+            btnFiltre[j].classList.remove('active');
+        }
+        this.classList.add('active');
+
+        console.log(items.textContent);
+    })
+}
+
