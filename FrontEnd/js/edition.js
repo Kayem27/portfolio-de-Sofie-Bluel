bc6026916a9fda679dd367d7552adc84e751f2f8
Kayem <abdoulatuf.pro@gmail.com>
2023-01-01 22:27:35 +0100

    feat: ajouter et supprimer un projet

--- FrontEnd/js/edition.js
+++ FrontEnd/js/edition.js
@@ -6,12 +6,14 @@ const logoutBtn = document.getElementById("logout");
 const editionMode = document.getElementById("editionMode");
 
 // Affichage des éléments d'édition
-if (myUser.token != undefined && myUser.token != "" && myUser.token != null) {
+if (myUser.token) {
   editionElements.forEach(function (elements) {
     elements.style.visibility = "visible";
   });
   loginNav.style.display = "none";
   editionMode.style.display = "flex";
+} else {
+  console.log("");
 }
 // ---------------------------------------
 // Déconnexion
@@ -20,27 +22,4 @@ logoutBtn.addEventListener("click", function () {
     window.localStorage.removeItem("myUser");
     location.reload(); 
   }
-});
-
-// -----------------------------------------------
-// Ajout de nouveaux projets
-const addWorkForm = document.getElementById("addWorkForm");
-
-addWorkForm.addEventListener("submit", function (event) {
-  event.preventDefault();
-  let newWork = new FormData(addWorkForm);
-  newWork.append("image", document.getElementById("image").files[0]);
-  newWork.append("title", document.getElementById("title").value);
-  newWork.append("category", categorySelect.options[categorySelect.selectedIndex].value);
-
-  fetch("http://localhost:5678/api/works/", {
-    method: "POST",
-    headers: {
-      "accept": "application/json",
-      "Authorization": "Bearer" + myUser.token,
-    },
-    body: newWork,
-  })
-    .then((response) => console.log(response))
-    .catch((err) => console.log(err));
 });
--- FrontEnd/js/script.js
+++ FrontEnd/js/script.js
@@ -1,12 +1,14 @@
 const gallery = document.getElementById("gallery");
 const editGallery = document.getElementById("editGallery");
-const WorksURL = "http://localhost:5678/api/works";
-const CategoryURL = "http://localhost:5678/api/categories";
 const categorySelect = document.getElementById("category");
-
+let myUser = JSON.parse(localStorage.getItem("myUser"));
 let items = document.querySelectorAll(".item");
 const theFiltre = document.getElementById("theFiltre");
 
+function reset() {
+  gallery.innerHTML = "";
+  editGallery.innerHTML = "";
+}
 const template = function (projets) {
   gallery.innerHTML += `
                 <figure class="item" category="${projets.category.id}">
@@ -20,14 +22,14 @@ const categoryTemplate = function (categorie) {
 const editGalleryTemplate = function (projets) {
   editGallery.innerHTML += `
       <figure>
-        <span id ="${projets.id}" class="deleteBtn"><i class="fa-solid fa-trash-can"></i></span>
+        <span data-id ="${projets.id}" class="deleteBtn"><i class="fa-solid fa-trash-can"></i></span>
   <!--  <span><i class="fa-solid fa-arrows-up-down-left-right"></i></span> -->
         <img src="${projets.imageUrl}" alt="${projets.title}">
         <figcaption id ="editBtn">éditer</figcaption>
       </figure>`;
 };
 // Les catégories
-fetch(CategoryURL)
+fetch("http://localhost:5678/api/categories")
   .then((response) => response.json())
   .then(function (categories) {
     theFiltre.innerHTML += `<li class="filtre active" categoryID="0">Tous</li>`;
@@ -54,49 +56,90 @@ fetch(CategoryURL)
   .catch((err) => console.error(error));
 
 // Récupération et affichage des projets
-fetch(WorksURL)
-  .then((res) => res.json())
-  .then(function (data) {
-    // Affichage des projets
-    for (let projets of data) {
-      template(projets);
-      editGalleryTemplate(projets);
-    }
-  })
-  // Le fonction de filtres
-  .then(function () {
-    const filtreBtn = document.querySelectorAll("#theFiltre li");
-    const works = document.querySelectorAll("#gallery figure");
+function render() {
+  fetch("http://localhost:5678/api/works")
+    .then((res) => res.json())
+    .then(function (data) {
+      // Affichage des projets
+      for (let projets of data) {
+        template(projets);
+        editGalleryTemplate(projets);
+      }
+    })
+    // Le fonction de filtres
+    .then(function () {
+      const filtreBtn = document.querySelectorAll("#theFiltre li");
+      const works = document.querySelectorAll("#gallery figure");
 
-    filtreBtn.forEach((li) => {
-      li.onclick = function () {
-        const value = li.getAttribute("categoryID");
-        works.forEach((figure) => {
-          figure.style.display = "none";
-          if (figure.getAttribute("category") === value || value === "0") {
-            figure.style.display = "block";
+      filtreBtn.forEach((li) => {
+        li.onclick = function () {
+          const value = li.getAttribute("categoryID");
+          works.forEach((figure) => {
+            figure.style.display = "none";
+            if (figure.getAttribute("category") === value || value === "0") {
+              figure.style.display = "block";
+            }
+          });
+        };
+      });
+    })
+    // Suppression d'un projet
+    .then(function () {
+      let deleteBtn = document.querySelectorAll(".deleteBtn");
+      deleteBtn.forEach((e) => {
+        e.onclick = function () {
+          if (confirm("êtes-vous sûr de vouloir supprimer ce projet ?")) {
+            let id = e.getAttribute("data-id");
+            fetch("http://localhost:5678/api/works/" + id, {
+              method: "DELETE",
+              headers: {
+                Authorization: "Bearer " + myUser.token,
+              },
+            }).then((response) => console.log(response))
+              .then(function(){
+                reset();
+                render();
+              })
           }
-        });
-      };
+        };
+      });
+    })
+    .catch((error) => {
+      console.log(error);
+      alert("Une erreur est survenue, veuillez réessayer ultérieurement !");
     });
+}
+render();
+
+// -----------------------------------------------
+// Ajout de nouveaux projets
+const addWorkForm = document.getElementById("addWorkForm");
+const modal = document.querySelector(".modal");
+
+addWorkForm.addEventListener("submit", function (event) {
+  event.preventDefault();
+  let newWork = new FormData();
+  newWork.append("image", document.getElementById("image").files[0]);
+  newWork.append("title", document.getElementById("title").value);
+  newWork.append(
+    "category",
+    categorySelect.options[categorySelect.selectedIndex].value
+  );
+
+  fetch("http://localhost:5678/api/works/", {
+    method: "POST",
+    headers: {
+      Accept: "application/json",
+      Authorization: "Bearer " + myUser.token,
+    },
+    body: newWork,
   })
-  // Suppression d'un projet
-  .then(function () {
-    let deleteBtn = document.querySelectorAll(".deleteBtn");
-    deleteBtn.forEach((e) => {
-      e.onclick = function () {
-        if (confirm("êtes-vous sûr de vouloir supprimer ce projet ?")) {
-          let id = e.getAttribute("id");
-          fetch("http://localhost:5678/api/works/" + id, {
-            method: "DELETE",
-          })
-          .then((response) => console.log(response)
-          )
-        }
-      };
-    });
-  })
-  .catch((error) => {
-    console.log(error);
-    alert("Une erreur est survenue, veuillez réessayer ultérieurement !");
-  });
+    .then((response) => console.log(response))
+    .then(function () {
+      addWorkForm.reset(); // réinitialise le formulaire
+      modal.classList.remove("show-modal");
+      reset();
+      render();
+    })
+    .catch((err) => console.log(err));
+});
